1e1193f546c56a4ac275b40ae52eda7f55ad4d30
Osgood Gunawan <osgoodgunawan@hotmail.com>
2019-12-03 03:22:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,9 +22,8 @@ function add2(n) {
   
 }
 var funkyFunction = function() {
-  return function() {
+ 
     return "FUNKY!"
-  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
