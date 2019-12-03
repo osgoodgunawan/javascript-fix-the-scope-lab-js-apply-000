5352d8a821c435e7ac94f798082e216de4fc39ae
Osgood Gunawan <osgoodgunawan@hotmail.com>
2019-12-03 03:13:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const n =498;
+  const two =2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
-  // const n = 841;
+  // const two = 2
+  // // const n = 841;
 }
 
 var funkyFunction = function() {
